6a54c1a7882f5f548ce59e388aad020bb2710f65
NUNORI <84273524+nunori@users.noreply.github.com>
2024-03-03 21:21:31 +0900

    [D3] Title: 성공적인 공연 기획, Time: 142 ms, Memory: 24,432 KB -BaekjoonHub

--- SWEA/D3/4789. 성공적인 공연 기획/성공적인 공연 기획.java
+++ SWEA/D3/4789. 성공적인 공연 기획/성공적인 공연 기획.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
@@ -10,22 +8,22 @@ public class Solution {
         int T = Integer.parseInt(br.readLine());
         for (int tc = 1; tc <= T; tc++) {
             String s = br.readLine();
-            int[] arr = new int[s.length()];
-            int[] arrsum = new int[arr.length];
-            int person=0;
-            for (int i = 0; i < s.length(); i++) {
-                arr[i] = s.charAt(i) - '0'; // 0번 인덱스부터 쌓기
-                if(i==0) arrsum[i] = arr[i];
-                else arrsum[i] = arrsum[i-1] + arr[i];
+            int[] arr = new int[s.length() + 1];
+            for (int i = 1; i <= s.length(); i++) {
+                arr[i] = s.charAt(i - 1) - '0';
             }
 
-            for (int i = 0; i < arr.length; i++) {
-                if(arrsum[i] < i + 1) {
-                    person = Math.max(person, i+1-arrsum[i]);
+            int sum = 0;
+            int need = 0;
+
+            for (int i = 1; i <= s.length(); i++) {
+                if (sum < i - 1) {
+                    need += (i - 1) - sum; // 필요한 사람 수 계산
+                    sum = i - 1; // 현재 박수 치는 사람의 수를 필요한 수로 업데이트
                 }
+                sum += arr[i]; // 현재 인덱스에서 박수 치는 사람 추가
             }
-
-            System.out.printf("#%d %d\n", tc, person);
+            System.out.printf("#%d %d\n", tc, need);
         }
     }
 }
