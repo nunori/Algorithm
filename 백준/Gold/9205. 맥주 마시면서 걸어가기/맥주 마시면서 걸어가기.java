b727a751c75921af1716c432a0464104998e10e5
NUNORI <84273524+nunori@users.noreply.github.com>
2024-04-14 15:40:21 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 148 ms, Memory: 14776 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -1,62 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static int[][] conv;
+    static boolean[] visited;
+    static boolean flag;
+    static int festX, festY, N;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
 
-        for(int tc = 1; tc <= T; tc++) {
-            int N = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            N = Integer.parseInt(br.readLine());
             st = new StringTokenizer(br.readLine());
             int homeX = Integer.parseInt(st.nextToken());
             int homeY = Integer.parseInt(st.nextToken());
-            int[][] conv = new int[N + 1][2];
+            conv = new int[N + 1][2];
 
-            for(int i = 0; i < N; i++) {
+            for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
                 conv[i][0] = Integer.parseInt(st.nextToken());
                 conv[i][1] = Integer.parseInt(st.nextToken());
             }
             st = new StringTokenizer(br.readLine());
-            int festX = Integer.parseInt(st.nextToken());
-            int festY = Integer.parseInt(st.nextToken());
+            festX = Integer.parseInt(st.nextToken());
+            festY = Integer.parseInt(st.nextToken());
 
             conv[N][0] = festX;
             conv[N][1] = festY;
 
-            boolean[] visited = new boolean[N + 1];
+            visited = new boolean[N + 1];
+            flag = false;
 
-            Queue<int[]> queue = new LinkedList<>();
-            queue.offer(new int[]{homeX, homeY});
+            dfs(homeX, homeY);
 
-            int flag = 0;
-            while (!queue.isEmpty()) {
-                int[] p = queue.poll();
-                if (p[0] == festX && p[1] == festY) {
-                    flag = 1;
-                    break;
-                }
-
-                for(int i = 0; i <= N; i++) {
-                    int distance = dist(p[0], p[1], conv[i][0], conv[i][1]);
-                    if (!visited[i] && distance <= 1000) {
-                        queue.offer(new int[]{conv[i][0], conv[i][1]});
-                        visited[i] = true;
-                    }
-                }
-            }
-            if(flag == 1)
+            if (flag)
                 System.out.println("happy");
             else
                 System.out.println("sad");
+        }
+    }
+
+    public static void dfs(int x, int y) {
+        if (x == festX && y == festY) {
+            flag = true;
+            return;
+        }
+
+        for (int i = 0; i <= N; i++) {
+            if (!visited[i] && dist(x, y, conv[i][0], conv[i][1]) <= 1000) {
+                visited[i] = true;
+                dfs(conv[i][0], conv[i][1]);
+            }
         }
     }
 
