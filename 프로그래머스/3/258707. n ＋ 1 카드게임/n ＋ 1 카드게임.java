1a33dbc51929e7e49ad5cabdc5fec09fd990d768
NUNORI <84273524+nunori@users.noreply.github.com>
2024-08-24 09:17:27 +0900

    [level 3] Title: n + 1 카드게임, Time: 15.37 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
@@ -1,55 +1,57 @@
 import java.util.*;
 class Solution {
-    Set<Integer> originalSet;
-    Set<Integer> addSet;
     public int solution(int coin, int[] cards) {
-        // 처음 set과 추가하려고 하는 addset
-        originalSet = new HashSet();
-        addSet = new HashSet();
+        Set<Integer> originalSet = new HashSet<>();
+        Set<Integer> addSet = new HashSet<>();
         int result = 0;
-        int goal = cards.length + 1;
-        int cardsCnt = cards.length / 3;
-        
-        for(int i = 0; i < cardsCnt; i++) {
+        int n = cards.length;
+        int goal = n + 1;
+        int firstDraw = cards.length / 3;
+        for(int i = 0; i < firstDraw; i++) {
             originalSet.add(cards[i]);
         }
         
         while(true) {
+            // 한 턴이 시작됐다는 것은 라운드 하나 진행한다는 것
             result++;
             
-            if(cardsCnt >= cards.length) break;
+            if(firstDraw >= n) break;
             
-            addSet.add(cards[cardsCnt]);
-            addSet.add(cards[cardsCnt + 1]);
-            cardsCnt += 2;
+            // 일단 카드 두 장 드로우 하고 시작
+            addSet.add(cards[firstDraw]);
+            addSet.add(cards[firstDraw + 1]);
+            firstDraw += 2;
             
-            // 있는 카드로 해결 가능할 때
+            // 해결 됐는지 판단하는 flag
             boolean flag = false;
+            
+            // 내가 가진 카드로 해결이 가능할 때
             for(int i : originalSet) {
                 if(originalSet.contains(goal - i)) {
-                    originalSet.remove(goal - i);
                     addSet.remove(i);
+                    originalSet.remove(goal - i);
                     flag = true;
                     break;
                 }
             }
             
-            // 카드 한 장 뽑아서 해결 가능할 때
+            // 카드 한 장 드로우 해서 해결할 때
             if(!flag) {
                 if(coin > 0) {
                     for(int i : originalSet) {
                         if(addSet.contains(goal - i)) {
-                            addSet.remove(goal - i);
                             originalSet.remove(i);
+                            addSet.remove(goal - i);
                             coin--;
                             flag = true;
                             break;
-                        }           
+                        }
                     }
                 }
+                
             }
             
-            // 카드 두 장 뽑아서 해결 가능할 때
+            // 카드 두 장 드로우 해서 해결할 때
             if(!flag) {
                 if(coin > 1) {
                     for(int i : addSet) {
@@ -64,12 +66,9 @@ class Solution {
                 }
             }
             
-            // 해결 가능하지 않을 때
+            // 어떻게 해도 해결이 안될 때
             if(!flag) break;
-            
         }
-        
         return result;
-        
     }
 }
