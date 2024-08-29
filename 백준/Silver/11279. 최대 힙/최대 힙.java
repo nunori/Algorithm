4fe8b5ae31a3c35f2d82875c9f20400a1723be38
NUNORI <84273524+nunori@users.noreply.github.com>
2024-08-29 19:34:21 +0900

    [Silver II] Title: 최대 힙, Time: 760 ms, Memory: 33548 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,28 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        PriorityQueue<Long> pQ = new PriorityQueue<>(Collections.reverseOrder());
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if(o1.equals(o2)) return 0;
+                else if(o1 > o2) return -1;
+                else return 1;
+            }
+        });
 
-        for (int tc = 1; tc <= N; tc++) {
-            long X = Integer.parseInt(br.readLine());
-            if (X > 0) {
-                pQ.offer(X);
-            } else {
-                if (pQ.isEmpty()) {
-                    System.out.println(0);
+        for(int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if(x == 0) {
+                if(!pq.isEmpty()) {
+                    System.out.println(pq.poll());
                 } else {
-                    System.out.println(pQ.poll());
+                    System.out.println(0);
                 }
+            }
+            else {
+                pq.add(x);
             }
         }
     }
