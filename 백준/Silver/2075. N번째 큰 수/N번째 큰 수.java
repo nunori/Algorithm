36de61c5230f7b5604235b12478e620ab5694267
NUNORI <84273524+nunori@users.noreply.github.com>
2024-08-29 18:41:32 +0900

    [Silver II] Title: N번째 큰 수, Time: 1328 ms, Memory: 231184 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N * N];
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++) {
+                arr[i * N + j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[arr.length - N]);
+    }
+}
