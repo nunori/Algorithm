d677f1bd0380702d5adc1378d3bde9f6de1b4cfd
NUNORI <84273524+nunori@users.noreply.github.com>
2024-09-15 18:46:28 +0900

    [level 3] Title: 야근 지수, Time: 124.86 ms, Memory: 68.1 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -2,31 +2,28 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] works) {
-        Arrays.sort(works);
-        int len = works.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int work : works) {
+            pq.offer(work);
+        }
         
         while (n > 0) {
-            int max = works[len - 1];
+            if (pq.isEmpty()) break;
             
-            if (max == 0) {
-                return 0;
+            int max = pq.poll();
+            
+            if (max > 0) {
+                max--;
+                n--;
+                pq.offer(max);
             }
-            
-            for (int i = len - 1; i >= 0; i--) {
-                if (works[i] == max && n > 0) {
-                    works[i]--;
-                    n--;
-                } else {
-                    break;
-                }
-            }
-            
-            Arrays.sort(works);
         }
         
         long sum = 0;
-        for (int i = 0; i < len; i++) {
-            sum += (long) works[i] * works[i];
+        while (!pq.isEmpty()) {
+            int work = pq.poll();
+            sum += (long) work * work;
         }
         
         return sum;
