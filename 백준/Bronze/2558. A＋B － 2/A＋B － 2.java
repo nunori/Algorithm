1a519f4e152a8baa9b587490c2f537f3aa0a62d6
NUNORI <84273524+nunori@users.noreply.github.com>
2024-09-23 23:31:53 +0900

    [Bronze V] Title: A+B - 2, Time: 172 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
