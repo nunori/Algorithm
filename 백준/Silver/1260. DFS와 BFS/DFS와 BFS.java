d61711567f7a532a0e0e3a8af40e204a559a541c
NUNORI <84273524+nunori@users.noreply.github.com>
2024-10-01 16:55:49 +0900

    [Silver II] Title: DFS와 BFS, Time: 288 ms, Memory: 21300 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -3,9 +3,9 @@ import java.io.*;
 
 public class Main {
     static int N, M, V;
-    static int[][] arr;
-    static boolean[] visited_bfs;
+    static ArrayList<Integer>[] graph;
     static boolean[] visited_dfs;
+    static boolean[] visited_bfs;
     static Queue<Integer> q = new LinkedList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,45 +14,53 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         V = Integer.parseInt(st.nextToken());
 
-        arr = new int[N + 1][N + 1];
-        visited_bfs = new boolean[N + 1];
-        visited_dfs = new boolean[N + 1];
-
-        for(int i = 1; i <= M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            arr[a][b] = 1;
-            arr[b][a] = 1;
+        graph = new ArrayList[N + 1];
+        for(int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
         }
+
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        for(int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        visited_dfs = new boolean[N + 1];
+        visited_bfs = new boolean[N + 1];
 
         dfs(V);
         System.out.println();
         bfs(V);
     }
 
-    static void bfs(int start) {
-        q.add(start);
-        visited_bfs[start] = true;
-        while(!q.isEmpty()) {
-            int curr = q.poll();
-            System.out.print(curr + " ");
-            for(int i = 1; i <= N; i++) {
-                if(arr[curr][i] == 1 && !visited_bfs[i]) {
-                    q.add(i);
-                    visited_bfs[i] = true;
-                }
+    private static void dfs(int node) {
+        visited_dfs[node] = true;
+        System.out.print(node + " ");
+        for(int next : graph[node]) {
+            if(!visited_dfs[next]) {
+                dfs(next);
             }
         }
     }
 
-    static void dfs(int start) {
-        visited_dfs[start] = true;
-        System.out.print(start + " ");
-
-        for(int i = 1; i <= N; i++) {
-            if(arr[start][i] == 1 && !visited_dfs[i])
-                dfs(i);
+    private static void bfs(int node) {
+        q.add(node);
+        visited_bfs[node] = true;
+        while(!q.isEmpty()) {
+            int next = q.poll();
+            System.out.print(next + " ");
+            for(int n : graph[next]) {
+                if(!visited_bfs[n]) {
+                    q.add(n);
+                    visited_bfs[n] = true;
+                }
+            }
         }
     }
 }
