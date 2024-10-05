6d2ffbcddef757f64de29f2b2418f2b3ad6dd9a7
NUNORI <84273524+nunori@users.noreply.github.com>
2024-10-05 16:44:08 +0900

    [level 2] Title: 주식가격, Time: 32.86 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,14 +1,20 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
         int[] res = new int[prices.length];
-        for(int i = 0; i < prices.length; i++) {
-            for(int j = i + 1; j < prices.length; j++) {
-                if(prices[i] > prices[j] || j == prices.length - 1) {
-                    res[i] = j - i;
-                    break;
-                }
+        Deque<Integer> deque = new ArrayDeque<>();
+        
+        for (int i = 0; i < prices.length; i++) {
+            while (!deque.isEmpty() && prices[i] < prices[deque.peekLast()]) {
+                int j = deque.pollLast();
+                res[j] = i - j;
             }
+            deque.offerLast(i);
+        }
+        while (!deque.isEmpty()) {
+            int j = deque.pollLast();
+            res[j] = prices.length - 1 - j;
         }
         return res;
     }
