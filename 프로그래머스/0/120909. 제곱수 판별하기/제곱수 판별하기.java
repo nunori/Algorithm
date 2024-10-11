dd9ab64969de61f70e42936fe06910e2bf416e84
NUNORI <84273524+nunori@users.noreply.github.com>
2024-10-12 02:21:26 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.05 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public int solution(int n) {
+        int sqrt = (int) Math.sqrt(n);
+        if(Math.pow(sqrt, 2) == n) return 1;
+        return 2;
+    }
+}
