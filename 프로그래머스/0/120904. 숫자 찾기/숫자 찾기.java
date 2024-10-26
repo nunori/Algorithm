12021182e779ebc2a6db609205d9aae67a3f1474
NUNORI <84273524+nunori@users.noreply.github.com>
2024-10-26 22:32:15 +0900

    [level 0] Title: 숫자 찾기, Time: 0.04 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,9 +1,8 @@
 class Solution {
     public int solution(int num, int k) {
         String s = Integer.toString(num);
-        char ch = Character.forDigit(k, 10);
         for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == ch) return i + 1;
+            if(s.charAt(i) == k + '0') return i + 1;
         }
         return -1;
     }
